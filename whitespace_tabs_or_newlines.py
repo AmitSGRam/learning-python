ae43b0e7ecb9fff60c2c38bbb9b4616988443434
AmitSGRam <67743441+AmitSGRam@users.noreply.github.com>
2020-07-08 04:25:58 +0530

    Add files via upload

---
+++ whitespace_tabs_or_newlines.py
@@ -0,0 +1,14 @@
+#Adding whitespace to strings with tabs or newlines
+print("\tRonaldo")
+print("\nRonaldo")
+print("\n \tRonaldo")
+print("London Bridge:\nIs\tFalling\tDown\nFalling Down")
+print("Games:\n\tCSGO\n\tGTA V\n\tEscape From Tarkov\n\tS.T.A.L.K.E.R.")
+fav_lang='	python	'
+print(fav_lang.lstrip())
+fav_lang=fav_lang.strip()
+print(fav_lang)
+msg="Python is Amit's favorite language"
+print(msg)
+sks='Deadlyslob says "SKS is my favorite"'
+print(sks)
