c65a20cbc7009b011450c7c3fa82a8461fd94b51
Amit Suresh <amit_suresh@outlook.com>
2022-12-16 05:04:21 +0530

    13_4_Raindrops

--- Raindrops/Randrops.py
+++ Raindrops/Randrops.py
@@ -8,8 +8,10 @@ from Raindrop import Raindrop
 from random import randint
 
 class Raindrops:
+    """Class to manage Raindrops game."""
     
     def __init__(self):
+        """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
         
@@ -22,13 +24,14 @@ class Raindrops:
         self._create_raindrop()
         
     def run_game(self):
+        """Start the main loop for the game."""
         while True:
             self._check_events()
             self._update_raindrops()
             self._update_display()
     
     def _update_display(self):
-        
+        """Insert image, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         
         self.raindrops.draw(self.screen)
@@ -36,6 +39,7 @@ class Raindrops:
         pygame.display.flip()
     
     def _check_events(self):
+        """Respond to keypresses."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -44,26 +48,32 @@ class Raindrops:
                     sys.exit()
     
     def _create_raindrop(self):
+        """Create raindrops on screen."""
         new_raindrop = Raindrop(self)
         #self.raindrops.add(new_raindrop)
         raindrop_width, raindrop_height = new_raindrop.rect.size
         
-        available_row_x = self.screen_width - (2 * raindrop_width)
-        number_x = available_row_x // (2 * raindrop_width)
+        available_row_x = self.screen_width - (2* raindrop_width)
+        self.number_x = available_row_x // (2 * raindrop_width)
         
-        available_row_y = self.screen_height - (2 * raindrop_height)
+        available_row_y = self.screen_height - (raindrop_height)
         number_y = available_row_y // (2 * raindrop_height)
         
         for rain_y in range(number_y):
-            for rain_x in range(number_x):
-                self._create_raindrops(rain_x, rain_y)
+            self._create_one_row(rain_y)
+    
+    def _create_one_row(self,rain_y):
+        """Create a row of raindrops."""
+        for rain_x in range(self.number_x):
+            self._create_raindrops(rain_x, rain_y)
                 
     def _create_raindrops(self,rain_x, rain_y):
+        """Create x raindrop and place in a row."""
         new_raindrop = Raindrop(self)
         raindrop_width, raindrop_height = new_raindrop.rect.size
-        new_raindrop.rect.x = raindrop_width + (2*raindrop_width*rain_x)
+        new_raindrop.rect.x = raindrop_width + (2 * raindrop_width * rain_x)
         
-        new_raindrop.y = raindrop_height * 2* rain_y
+        new_raindrop.y = raindrop_height * 2 * rain_y
         new_raindrop.rect.y = new_raindrop.y
         
         new_raindrop.rect.x += randint(-10,10)
@@ -78,8 +88,11 @@ class Raindrops:
         create_new_raindrops = False
         for raindrop in self.raindrops.copy():
             if raindrop.rect.top>self.screen_rect.bottom:
-                raindrop.remove()
+                self.raindrops.remove(raindrop)
                 create_new_raindrops = True
+        
+        if create_new_raindrops:
+            self._create_one_row(0)
     
 if __name__ == '__main__':
     raindrops = Raindrops()
